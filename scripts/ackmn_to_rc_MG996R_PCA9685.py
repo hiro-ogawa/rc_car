6b778520eff5ac917e86a8503ccbc2e6b0778eb8
sngc1 <strngch@gmail.com>
2019-11-02 21:37:43 +0900

    Update pwm control

--- scripts/ackmn_to_rc_MG996R_PCA9685.py
+++ scripts/ackmn_to_rc_MG996R_PCA9685.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import math
 import time
+
 import rospy
 from ackermann_msgs.msg import AckermannDrive
 
@@ -8,28 +10,66 @@ import Adafruit_PCA9685
 CHANNEL_STEER = 0
 CHANNEL_DRIVE = 1
 
-# Servo pulse lengths (out of 4096)
 STEER_MIN = 230
-STEER_MAX = 520
+STEER_MAX = 430
 STEER_RANGE = STEER_MAX - STEER_MIN
-STEER_NEUTRAL = STEER_RANGE // 2 + STEER_MIN
-DRIVE_MIN = 250
-DRIVE_MAX = 500
-DRIVE_RANGE = DRIVE_MAX - DRIVE_MIN
-DRIVE_NEUTRAL = DRIVE_RANGE // 2 + DRIVE_MIN
+STEER_NEUTRAL = 340
+
+MAX_STEER_ANGLE = math.radians(20)
+
+
+DRIVE_NEUTRAL = 375
+
+DRIVE_BACK_MAX = 250 
+DRIVE_BACK_RANGE = DRIVE_NEUTRAL - DRIVE_BACK_MAX
+
+DRIVE_FWRD_MAX = 450
+DRIVE_FWRD_RANGE = DRIVE_FWRD_MAX - DRIVE_NEUTRAL
+
+# empirical max speed on DRIVE_FWRD_MAX
+MAX_SPEED = 8.3  # m/s = 30km/h  
+
+SPEED_LIMI = MAX_SPEED  # no limit if max speed
 
 pwm = Adafruit_PCA9685.PCA9685()  # default i2c address: 0x40
 pwm.set_pwm_freq(60)  # Hz
 
-def to_pwm(drive, steer):
-    print('drive=', drive, 'steer=', steer)
+JOY = False  # if True, input value is within the range of -1.0 - 1.0
 
-    if drive > 0.1:
-        drive = 0.1
+
+def normalize_drive(speed):
+    """ convert speed m/s to the range from 0 to 1.0
+    """
+    return speed/MAX_SPEED
+
+def normalize_steer(angle_rad):
+    """ convert speed m/s to the range from 0 to 1.0
+    """
+    return angle_rad/MAX_STEER_ANGLE
+
+def to_pwm(drive, steer):
+    print('drive %s, steer=%s', drive, math.degrees(steer))
+
+    #if drive > 0.15:
+    #    drive = 0.15
+    if not JOY:
+        drive = normalize_drive(drive)
+        steer = normalize_steer(steer)
 
     steer_pwm = STEER_NEUTRAL - int(steer * STEER_RANGE)//2
-    drive_pwm = DRIVE_NEUTRAL + int(drive * DRIVE_RANGE)//2
- 
+
+    if drive > 0:
+        drive_pwm = DRIVE_NEUTRAL + int(drive * DRIVE_FWRD_RANGE)
+    else: 
+        drive_pwm = DRIVE_NEUTRAL - int(abs(drive) * DRIVE_BACK_RANGE)
+
+    if steer_pwm > STEER_MAX:
+        steer_pwm = STEER_MAX
+    if steer_pwm < STEER_MIN:
+        steer_pwm = STEER_MIN
+    
+    #print('drive pwm', drive_pwm)
+    #print('steer pwm', steer_pwm)
     return drive_pwm, steer_pwm
 
 def callback(data):
